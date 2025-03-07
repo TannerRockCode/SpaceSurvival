cda420df0b85c5422ca81ca4ca0cc7e225b73f0d
athorne <adamreidthorne@gmail.com>
2025-03-07 07:05:03 -0800

    working on collidables

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 	"log"
 	"math"
@@ -13,40 +14,52 @@ import (
 )
 
 const (
-	screenWidth = 960
-	screenHeight = 540
-	asteroidSpeed = 0.9
-) 
+	screenWidth       = 960
+	screenHeight      = 540
+	asteroidSpeed     = 0.9
+	collisionGridSize = 5
+)
 
-
-//var startTime = time.Now()
+// var startTime = time.Now()
 var lastEnemySpawn = time.Now()
 
-type Game struct{
-	player Player
-	lasers []Laser
-	asteroids []Asteroid
+type Game struct {
+	player       Player
+	lasers       []Laser
+	asteroids    []Asteroid
+	collisionMap CollisionMap
 }
 
-type Laser struct{
-	x float64 
-	y float64
-	rad float64
-	dirX float64
-	dirY float64
-	beam *ebiten.Image
-	width int
+type CollisionMap struct {
+	grid map[string][]Collidable
+}
+
+type Laser struct {
+	x      float64
+	y      float64
+	rad    float64
+	dirX   float64
+	dirY   float64
+	beam   *ebiten.Image
+	width  int
+	height int
+	usedUp bool
+}
+
+type Asteroid struct {
+	x      float64
+	y      float64
+	dirX   float64
+	dirY   float64
+	sprite *ebiten.Image
+	width  int
 	height int
 }
 
-type Asteroid struct{
-	x float64
-	y float64
-	dirX float64
-	dirY float64
-	sprite *ebiten.Image
-	width int
-	height int 
+func (c *CollisionMap) Clear() {
+	for i := range c.grid {
+		clear(c.grid[i])
+	}
 }
 
 func (l *Laser) createBeam() {
@@ -59,7 +72,7 @@ func (l *Laser) Draw(screen *ebiten.Image) {
 	geo.Translate(-float64(l.width)/2, -float64(l.height)/2)
 	geo.Rotate(l.rad)
 	geo.Translate(l.x, l.y)
-	op := &ebiten.DrawImageOptions{GeoM:geo}
+	op := &ebiten.DrawImageOptions{GeoM: geo}
 	screen.DrawImage(l.beam, op)
 }
 
@@ -72,7 +85,7 @@ func (g *Game) DrawLasers(screen *ebiten.Image) {
 func (a *Asteroid) Draw(screen *ebiten.Image) {
 	geo := ebiten.GeoM{}
 	geo.Translate(a.x, a.y)
-	op := &ebiten.DrawImageOptions{GeoM:geo}
+	op := &ebiten.DrawImageOptions{GeoM: geo}
 	screen.DrawImage(a.sprite, op)
 }
 
@@ -83,18 +96,33 @@ func (g *Game) DrawAsteroids(screen *ebiten.Image) {
 }
 
 func (l Laser) IsOffScreen() bool {
-	return l.x < 0 || l.x > float64(screenWidth) || l.y < 0 || l.y > float64(screenHeight) 
+	return l.x < 0 || l.x > float64(screenWidth) || l.y < 0 || l.y > float64(screenHeight)
 }
 
 func (g *Game) CleanLasers() {
 	i := 0
 	for _, v := range g.lasers {
-		if !v.IsOffScreen() {
+		if !v.IsOffScreen() && !v.usedUp {
 			g.lasers[i] = v
 			i++
 		}
 	}
 	g.lasers = g.lasers[:i]
+}
+
+func (a Asteroid) IsOffScreen() bool {
+	return a.x < 0 || a.x > float64(screenWidth) || a.y < 0 || a.y > float64(screenHeight)
+}
+
+func (g *Game) CleanAsteroids() {
+	i := 0
+	for _, a := range g.asteroids {
+		if !a.IsOffScreen() {
+			g.asteroids[i] = a
+			i++
+		}
+	}
+	g.asteroids = g.asteroids[:i]
 }
 
 func (g *Game) MovePlayer() {
@@ -120,34 +148,85 @@ func (g *Game) MovePlayer() {
 
 func (g *Game) PlayerShoot() {
 	if ebiten.IsKeyPressed(ebiten.KeySpace) {
-		laser := Laser {
-			x : g.player.x,
-			y : g.player.y,
-			rad : g.player.rad,
-			dirX : g.player.dirX * 1.3,
-			dirY : g.player.dirY * 1.3,
-			width : 1,
-			height : 3,
+		laser := Laser{
+			x:      g.player.x,
+			y:      g.player.y,
+			rad:    g.player.rad,
+			dirX:   g.player.dirX * 1.3,
+			dirY:   g.player.dirY * 1.3,
+			width:  1,
+			height: 3,
+			usedUp: false,
 		}
 		laser.createBeam()
 		g.lasers = append(g.lasers, laser)
 	}
 }
 
+type Collidable interface {
+	//registerWithCollisionMap()
+	//detectCollision(Collidable, Collidable) bool
+	GetBounds() image.Rectangle
+	HandleCollision(Collidable)
+}
+
+func (l Laser) GetBounds() image.Rectangle {
+	return l.beam.Bounds()
+}
+
+func (a Asteroid) GetBounds() image.Rectangle {
+	return a.sprite.Bounds()
+}
+
+func detectCollision(c1, c2 Collidable) bool {
+	b1 := c1.GetBounds()
+	b2 := c2.GetBounds()
+	return b1.Overlaps(b2)
+}
+
+func (l Laser) HandleCollision(c Collidable) {
+	l.usedUp = true
+}
+
+func (a *Asteroid) HandleCollision(c Collidable) {
+	_, ok := c.(Laser)
+	if ok {
+		a.sprite.Fill(color.RGBA{50, 0, 0, 255})
+	}
+}
+
+//for each value in key, for each other value in key if(detectCollision(key1, key2) then key1.handleCollision(key2) key2.handleCollision(key1)
+
+// hashmap grid 96 columns 54 rows - key, which is a combo of the column and row - value is a slice of collidable objects
+// register. For every object in a key, detect collision.
+//for every value in a key, check to see if that value is colliding with the other values
+//c.detectCollision(c2)
+
 func (g *Game) Update() error {
+	g.collisionMap.Clear()
 	g.LaserShoot()
 	g.MoveLasers()
+	g.MoveAsteroids()
+	asteroidCollidables := make([]Collidable, len(g.asteroids))
+	for i := range g.asteroids {
+		asteroidCollidables[i] = &g.asteroids[i]
+	}
+	laserCollidables := make([]Collidable, len(g.lasers))
+	for i := range g.lasers {
+		laserCollidables[i] = &g.lasers[i]
+	}
+	g.registerCollidables(append(asteroidCollidables, laserCollidables...))
 	g.CleanLasers()
 	g.MovePlayer()
 	g.PlayerShoot()
 	g.SpawnAsteroid()
-	g.MoveAsteroids()
+	g.CleanAsteroids()
 
 	return nil
 }
 
 func (g *Game) SpawnAsteroid() {
-	if time.Since(lastEnemySpawn) > 1 * time.Second {
+	if time.Since(lastEnemySpawn) > 1*time.Second {
 		lastEnemySpawn = time.Now()
 		//random border location
 		xLocation := 0
@@ -171,20 +250,20 @@ func (g *Game) SpawnAsteroid() {
 			}
 			xLocation = rand.Intn(screenWidth + 1)
 		}
-		
+
 		playerXDist := (g.player.x - float64(xLocation))
-		playerYDist := (g.player.y - float64(yLocation)) 
+		playerYDist := (g.player.y - float64(yLocation))
 		asteroidDividend := (playerXDist + playerYDist) / asteroidSpeed
 		asteroidWidth := rand.Intn(40) + 10
 		asteroidHeight := rand.Intn(40) + 10
 
 		asteroid := Asteroid{
-			x: float64(xLocation), 
-			y: float64(yLocation), 
-			dirX: (playerXDist / asteroidDividend), 
-			dirY: (playerYDist / asteroidDividend), 
+			x:      float64(xLocation),
+			y:      float64(yLocation),
+			dirX:   (playerXDist / asteroidDividend),
+			dirY:   (playerYDist / asteroidDividend),
 			sprite: ebiten.NewImage(asteroidWidth, asteroidHeight),
-			width: asteroidWidth,
+			width:  asteroidWidth,
 			height: asteroidHeight,
 		}
 		asteroid.sprite.Fill(color.RGBA{177, 10, 75, 255})
@@ -210,7 +289,7 @@ func (g *Game) MoveLasers() {
 }
 
 func (g *Game) LaserShoot() {
-	currLaser := Laser {
+	currLaser := Laser{
 		x: 0,
 		y: 0,
 	}
@@ -224,10 +303,28 @@ func (g *Game) LaserShoot() {
 func (l *Laser) Update() {
 	l.x += l.dirX
 	l.y -= l.dirY
-	
+}
+func (g *Game) registerCollidables(c []Collidable) {
+	for i := range c {
+		g.registerWithCollisionMap(c[i])
+	}
+}
+func (g *Game) registerWithCollisionMap(c Collidable) {
+	// width is 7
+	b := c.GetBounds()
+	minX := b.Min.X - b.Min.X%collisionGridSize                     //if 7, then start value is 5, cause round down
+	minY := b.Min.Y - b.Min.Y%collisionGridSize                     //round down
+	maxX := b.Max.X + collisionGridSize - b.Max.X%collisionGridSize // 14 round up to next 15 value 14 % 5 5-4
+	maxY := b.Max.Y + collisionGridSize - b.Max.Y%collisionGridSize // round up
+	for i := minX; i <= maxX; i += collisionGridSize {
+		for j := minY; j <= maxY; j += collisionGridSize {
+			key := fmt.Sprintf("%d:%d", i, j)
+			g.collisionMap.grid[key] = append(g.collisionMap.grid[key], c)
+		}
+	}
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {	
+func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("Ticks Per Second: %0.2f", ebiten.ActualTPS()))
 	g.player.Draw(screen)
 	g.DrawLasers(screen)
@@ -242,12 +339,12 @@ func main() {
 	fmt.Printf("I am drawing!")
 	ebiten.SetWindowSize(1920, 1080)
 	player := Player{
-		height : 10,
-		width : 10,
-		dirX: 0,
-		dirY: 1,
-		x : 475,
-		y : 265,
+		height: 10,
+		width:  10,
+		dirX:   0,
+		dirY:   1,
+		x:      475,
+		y:      265,
 	}
 	lasers := make([]Laser, 0, 500)
 	asteroids := make([]Asteroid, 0, 25)
@@ -255,7 +352,16 @@ func main() {
 	player.character.Fill(color.White)
 	ebiten.SetVsyncEnabled(true)
 	ebiten.SetTPS(ebiten.SyncWithFPS)
-	if err := ebiten.RunGame(&Game{player, lasers, asteroids}); err != nil {
+	cMap := CollisionMap{
+		grid: make(map[string][]Collidable),
+	}
+	for i := screenWidth; i >= 0; i -= collisionGridSize {
+		for j := screenHeight; j >= 0; j -= collisionGridSize {
+			cMap.grid[fmt.Sprintf("%d:%d", i, j)] = make([]Collidable, 0, 100)
+		}
+	}
+	//
+	if err := ebiten.RunGame(&Game{player, lasers, asteroids, cMap}); err != nil {
 		log.Fatal(err)
 	}
 
